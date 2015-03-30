c89c9e32ef32c7692b9e5e537d08bd052f6834e4
Ryo Miyake <ryo.studiom@gmail.com>
2015-03-30 11:51:30 +0900

    Support web front-proxy through $FRONT_PROXY

--- start.sh
+++ start.sh
@@ -6,7 +6,9 @@ service gearman-job-server start
 ./cloudforecast_radar -c config/cloudforecast.yaml -l config/server_list.yaml &
 ./cf_fetcher_worker -r -c config/cloudforecast.yaml -max-workers 2 -max-request-per-child 100 -max-exection-time 60 &
 ./cf_updater_worker -r -c config/cloudforecast.yaml -max-workers 2 -max-request-per-child 100 -max-exection-time 60 &
-./cloudforecast_web -p 5000 -c config/cloudforecast.yaml -l config/server_list.yaml &
+
+[ ! -z "$FRONT_PROXY" ] && FRONT_PROXY="--front-proxy=${FRONT_PROXY}"
+./cloudforecast_web -p 5000 ${FRONT_PROXY} -c config/cloudforecast.yaml -l config/server_list.yaml &
 
 trap '
 service gearman-job-server stop;
